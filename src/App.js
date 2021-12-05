d40b2c1f041a1aa25e348eb96f38045910604cd1
SolbiShin <sbshin.dev@gmail.com>
2021-12-05 21:16:07 +0900

    Suspense를 사용하여 코드 스플리팅하기

--- src/App.js
+++ src/App.js
@@ -1,29 +1,24 @@
-import React, {Component} from 'react';
+import React, {useState, Suspense} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  state = {
-   splitMe: null
+const SplitMe = React.lazy(() => import('./SplitMe'));
+function App () {
+  const [visible, setVisible] = useState(false);
+  const onClick = () => {
+    setVisible(true);
   };
-  handleClick = async () => {
-    const loadedModule = await import('./SplitMe');
-    this.setState({
-      SplitMe: loadedModule.default
-    });
-  };
-  render() {
-    const {SplitMe} = this.state;
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p onClick={this.handleClick}>Hello React!</p>
-          {SplitMe && <SplitMe />}
+          <p onClick={onClick}>Hello React!</p>
+          <Suspense fallback={<div>loading...</div>}>
+            {visible && <SplitMe />}
+          </Suspense>
         </header>
       </div>
     );
-  }
 }
 
 export default App;
